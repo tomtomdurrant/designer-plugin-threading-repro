9ea93a2af978005876a2c02a976c4e662cc57b63
Tom Whittock <tom.whittock@disguise.one>
2025-03-27 15:30:59 +0000

    Fix being hosted as as a d3 plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: './', // Use relative URLs for assets, so it works when hosted in designer
   plugins: [vue()],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.vue']
