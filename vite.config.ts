b142ad45f1ab688399273ad80357b0b07c55b49b
Tom Whittock <tom.whittock@disguise.one>
2025-03-31 12:07:03 +0000

    Add a basic temporary icon...

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { copyFileSync } from 'fs';
 
 export default defineConfig({
   base: './', // Use relative URLs for assets, so it works when hosted in designer
@@ -9,5 +10,17 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0'
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'copy-extra-assets',
+          generateBundle() {
+            copyFileSync('icon.svg', 'dist/icon.svg');
+          }
+        }
+      ]
+    }
   }
 });
